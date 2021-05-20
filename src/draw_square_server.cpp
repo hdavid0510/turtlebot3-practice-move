da02d93b5c084142b62b1c016095ca379ca32bb9
David Hong <gdavid0510@gmail.com>
2021-05-20 18:08:48 +0900

    odom accuracy adjusting

--- src/draw_square_server.cpp
+++ src/draw_square_server.cpp
@@ -1,15 +1,17 @@
 #include <ros/ros.h>
-#include <math.h>
+#include <cmath>
 #include <geometry_msgs/Twist.h>
-#include <string.h>
+#include <cstring>
+#include <nav_msgs/Odometry.h>
+#include <tf/tf.h>
 
 // srv폴더에 만든 서비스 파일에서 생성되는 헤더파일로 catkin_make 실행 중 생성된다.
 #include "turtlebot3_practice_move/square.h"
 
 
 // 기본 속도; 너무 빠르면 전진 거리/회전 각도가 부정확해진다.
-const double speed = 0.5;
-const double angular_speed = 0.5;
+const double default_forward_speed = 0.5;
+const double default_angular_speed = 1;
 const int polygon = 4;
 
 // Turtlebot3 Limitations
@@ -27,6 +29,19 @@ std::string turtlebot3_model;
 // main함수만이 아닌 Service Callback 함수에서도 publisher를 참조해야 하므로 전역 변수로 설정
 // NodeHandler를 전역변수로 설정할 경우 ros::init이 실행되기 전 초기화를 시도하면서 오류가 발생한다.
 ros::Publisher pub;
+
+// Odometry Subscription
+ros::Subscriber sub_tb3_odom;
+nav_msgs::Odometry::ConstPtr tb3_odom;
+
+// 그리기 옵션들
+float side_length = 0;
+int32_t rotations = 0;
+int currentDrawingLine = -1;// 현재 그리고 있는 사각형 몇 개 선분 완성됐는지, -1이면 쉬고 있는 상태
+bool isTurning = false; // 사각형 그리는 중, 현재 회전중인지
+double initPositionX = 0;
+double initPositionY = 0;
+double initRotationZ = 0;
 
 
 const float makeSimpleProfile(float output, float input, float slop){
@@ -73,99 +88,101 @@ const float checkAngularLimitVelocity(float vel){
 	return vel;
 }
 
+const double pointDistance(double x0, double y0, double x1, double y1)
+{
+	return sqrt(pow(x0-x1,2)+pow(y0-y1,2));
+}
 
+const double getAngleFromOdom(nav_msgs::Odometry::ConstPtr &odom, double &roll, double &pitch, double &yaw){
+	tf::Quaternion q(odom->pose.pose.orientation.x, odom->pose.pose.orientation.y, odom->pose.pose.orientation.z, odom->pose.pose.orientation.w);
+	tf::Matrix3x3 m(q);
+	m.getRPY(roll, pitch, yaw);
+	return yaw;
+}
 
-// 직선 그리기 (side_length=변 길이; &vel_msg=속도 메시지(토픽); &pub=vel_msg 퍼블리셔)
-void draw_in_line(float side_length, geometry_msgs::Twist &vel_msg, ros::Publisher &pub)
+const double angleDistance(double y_final, double y_init){
+	double result = y_final - y_init;
+	return ((result < 0) ? result + 2*M_PI : result) ;
+}
+
+const float smoothAngularSpeed(double y_target, double y_current, double threshold, double angular_speed)
+{
+	if (angleDistance(y_target, y_current) >= threshold){
+		return angular_speed;
+	}else{
+		return angular_speed/5 * (4 + angleDistance(y_target, y_current) / threshold);
+	}
+}
+
+// 직선 그리기 (&vel_msg=속도 메시지(토픽); &pub=vel_msg 퍼블리셔; forward_speed=병진 속도 )
+void forward(geometry_msgs::Twist &vel_msg, ros::Publisher &pub, float forward_speed)
 {
 	// vel_msg에 포함된 3차원 속도 값 초기화
-	vel_msg.linear.x = speed; //turtle은 전진만 하므로 x를 제외한 나머지 속도를 0으로 맞춘다.
+	vel_msg.linear.x = forward_speed; //turtle은 전진만 하므로 x를 제외한 나머지 속도를 0으로 맞춘다.
 	vel_msg.linear.y = 0;
 	vel_msg.linear.z = 0;
 	vel_msg.angular.x = 0;
 	vel_msg.angular.y = 0;
 	vel_msg.angular.z = 0;
 
-	// 출발 시각
-	double t0 = ros::Time::now().toSec();
-	// 이동 거리
-	double distance_travelled = 0;
+	pub.publish(vel_msg);
+	// ROS_INFO("x:%f y:%f z:%f", tb3_odom->pose.pose.position.x, tb3_odom->pose.pose.position.y, tb3_odom->pose.pose.position.z);
+}
 
-	while (distance_travelled < side_length) // 이동 거리가 아직 변 길이에 미치지 못했을 경우
-	{
-		// 속도 메시지 전송(전진 명령)
-		pub.publish(vel_msg);
-		// 짧은 이동 시간 측정하여 이동 거리(=속도x시간) 계산
-		double t1 = ros::Time::now().toSec();
-		distance_travelled = speed * (t1 - t0);
-	}
-	// 전진 속도 0으로 변경; 정지 명령
+// 직각 회전하기 (&vel_msg=속도 메시지(토픽); &pub=vel_msg 퍼블리셔; angular_speed=각속도)
+void rotate(geometry_msgs::Twist &vel_msg, ros::Publisher &pub, float angular_speed)
+{
+	// vel_msg에 포함된 3차원 속도 값 중 각속도만 초기화
 	vel_msg.linear.x = 0;
+	vel_msg.linear.y = 0;
+	vel_msg.linear.z = 0;
+	vel_msg.angular.x = 0;
+	vel_msg.angular.y = 0;
+	vel_msg.angular.z = angular_speed;
+
 	pub.publish(vel_msg);
 }
 
-// 직각 회전하기 (&vel_msg=속도 메시지(토픽); &pub=vel_msg 퍼블리셔)
-void rotate(geometry_msgs::Twist &vel_msg, ros::Publisher &pub)
+// STOP
+void stop(geometry_msgs::Twist &vel_msg, ros::Publisher &pub)
 {
-	// vel_msg에 포함된 3차원 속도 값 중 각속도만 초기화 (나머지 속도값은 draw_in_line에서 초기화됨)
-	vel_msg.angular.z = angular_speed;
-
-	// 출발 시각
-	double t0 = ros::Time::now().toSec();
-
-	// 회전한 각도
-	double angle_travelled = 0;
-
-	// 회전할 각도: 90deg = pi/2
-	// const double turn_angle = M_PI/2.0;
-	const double turn_angle = M_PI * 2 / polygon;
-
-	while (angle_travelled < turn_angle) // 회전한 각도가 목표 각도에 미치지 못했을 경우
-	{
-		// 속도 메시지 전송(전진 명령)
-		pub.publish(vel_msg);
-		// 짧은 회전 시간 측정하여 회전 각도(=각속도x시간) 계산
-		double t1 = ros::Time::now().toSec();
-		angle_travelled = angular_speed * (t1 - t0);
-	}
-	// 각속도 0으로 변경; 정지 명령
+	// vel_msg에 포함된 3차원 속도 값 중 각속도만 초기화
+	vel_msg.linear.x = 0;
+	vel_msg.linear.y = 0;
+	vel_msg.linear.z = 0;
+	vel_msg.angular.x = 0;
+	vel_msg.angular.y = 0;
 	vel_msg.angular.z = 0;
+
 	pub.publish(vel_msg);
 }
 
 // 사각형 그리기 서비스 Request에 응답하는 Callback 함수
 bool handlesquare(turtlebot3_practice_move::square::Request &req, turtlebot3_practice_move::square::Response &res)
 {
+	//TODO: 이미 그리고 있는데 그리기 명령이 들어오는 것 처리하기
 
 	ROS_INFO("[draw_square_server] I just got incoming request!");
 
-	// vel_msg 메시지 객체 생성
-	geometry_msgs::Twist vel_msg;
-
 	// service client로부터 요청된 변 길이 받아오기
-	float side_length = req.sidelength;
+	side_length = req.sidelength;
 	ROS_INFO_STREAM("[draw_square_server] req.sidelength=" << req.sidelength);
 
 	// service client로부터 요청된 반복 회수 받아오기
-	int32_t rotations = req.rotations;
+	rotations = req.rotations;
 	ROS_INFO_STREAM("[draw_square_server] req.rotations=" << req.rotations);
 
-	// 현재 90도(quarter)를 몇 번 돌았는지 확인
-	// = (요청된 반복 횟수)*4
-	int current_rotation_quarter = 0;
-	while (current_rotation_quarter < rotations * polygon && ros::ok())
-	{
-		// 디버그 정보 출력
-		ROS_DEBUG_STREAM("[draw_square_server] current_rotation_quarter, rotations=" << current_rotation_quarter << ", " << rotations);
-
-		// 직선 그리고 회전, 카운터 증가
-		draw_in_line(side_length, vel_msg, pub);
-		rotate(vel_msg, pub);
-		current_rotation_quarter++;
-	}
+	// 그리기 시작
+	currentDrawingLine = 0;
+	initPositionX = tb3_odom->pose.pose.position.x;
+	initPositionY = tb3_odom->pose.pose.position.y;
 
 	// 여기서 return값에 의미는 없으나 콜백함수 형식을 바꿀 수 없으므로 return 값을 임의로 true로 설정
 	return true;
+}
+
+void tb3_odom_callback(const nav_msgs::Odometry::ConstPtr &msg){
+	tb3_odom = msg;
 }
 
 int main(int argc, char **argv)
@@ -182,6 +199,8 @@ int main(int argc, char **argv)
 	pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 	ROS_INFO("[draw_square_server] Advertising publisher /cmd_vel");
 
+	sub_tb3_odom = nh.subscribe("odom", 1000, tb3_odom_callback);
+
 	// Turtlebot3 모델 정보 받아오기
 	//turtlebot3_model = rospy.get_param("model", "burger");
 	// turtlelbot3_model = nh.getParam();
@@ -191,9 +210,63 @@ int main(int argc, char **argv)
 	ros::ServiceServer s = nh.advertiseService<turtlebot3_practice_move::square::Request, turtlebot3_practice_move::square::Response>("draw_square", handlesquare);
 	ROS_INFO("[draw_square_server] Advertising service server draw_square");
 
+	// vel_msg 메시지 객체 생성
+	geometry_msgs::Twist vel_msg;
+
 	// CTRL+C 등 종료 명령 받기 위해 ros::spin()대신 ros::ok(), 루프, ros::spinOnce() 조합
-	while (ros::ok())
+	int logCount = 0;
+	while (ros::ok()){
+		// 콜백함수 대기 등
 		ros::spinOnce();
+
+		if(0 <= currentDrawingLine && currentDrawingLine < polygon){
+			if(isTurning){
+				// 회전중
+				double roll, pitch, yaw;
+				getAngleFromOdom(tb3_odom, roll, pitch, yaw);
+				if (angleDistance(yaw, initRotationZ) >= (M_PI * 2 / polygon))
+				{
+					//회전 종료
+					stop(vel_msg, pub);
+					isTurning = false;
+					currentDrawingLine++;
+					if(currentDrawingLine >= polygon){
+						//완성
+						currentDrawingLine = -1;
+						stop(vel_msg, pub);
+					}else{
+						//직진 시작점 저장
+						initPositionX = tb3_odom->pose.pose.position.x;
+						initPositionY = tb3_odom->pose.pose.position.y;
+					}
+				}
+				else
+				{
+					rotate(vel_msg, pub, smoothAngularSpeed(initRotationZ+(M_PI * 2 / polygon), yaw, 30*M_PI/180, default_angular_speed));
+					logCount++;
+					if (logCount >= 30){
+						ROS_INFO("yaw:%f", yaw);
+						logCount = 0;
+					}
+				}
+			}else{
+				// 직진중
+				if (pointDistance(initPositionX, initPositionY, tb3_odom->pose.pose.position.x, tb3_odom->pose.pose.position.y) >= side_length)
+				{
+					//직진 종료
+					isTurning = true;
+					//회전 시작점 저장
+					double roll, pitch, yaw;
+					getAngleFromOdom(tb3_odom, roll, pitch, yaw);
+					initRotationZ = yaw;
+				}
+				else
+				{
+					forward(vel_msg, pub, default_forward_speed);
+				}
+			}
+		}
+	}
 
 	return 0;
 }
